682bf78376ca2e675134abe6c57c96b8069a9360
mohdraqif <mohdraqif94@gmail.com>
2019-07-24 00:48:08 +0530

    Init commit

--- src/index.js
+++ src/index.js
@@ -1,46 +1,70 @@
-// " on " -> data "in" to a system
-// " emit " -> data "out" from a system
-// " io " -> data transfer to all systems
-
-const express = require('express')
-const app = express()
 const path = require('path')
 const http = require('http')
+const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
-const { generateMessage }=  require('./utils/messages')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.use(express.static(publicDirectoryPath))
 
-const server = http.createServer(app)  //creates a new http server
-const io = socketio(server)            //converts 'that' server into websocket server
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
 
-app.use(express.static(path.join(__dirname, '../public')))
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options })
 
-
-io.on('connection', (socket) =>{
-
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.emit('message', generateMessage('mohd.raqif joined the conversation!'))
-
-    socket.on('sendMessage', (message, callback) =>{
-        const filter = new Filter()
-
-        if(filter.isProfane(message)) {
-            return callback("Be Careful With Your Words!")
+        if (error) {
+            return callback(error)
         }
-        
-        io.emit('message', generateMessage(message))
-        callback()
-    }) 
 
-    socket.on('location', (coords, callback) =>{
-        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}.` )
+        socket.join(user.room)
+
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+
         callback()
     })
 
-    socket.on('disconnect', () =>{
-        io.emit('message', generateMessage('mohd.raqif left the conversation!'))
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.to(user.room).emit('message', generateMessage(user.username, message))
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
     })
 })
 
